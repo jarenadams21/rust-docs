64edf6139fbfc4e212aadcf99455a2780ce65bec
jarenadams21 <jarenadams3@gmail.com>
2023-03-24 13:30:36 -0400

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rust Docs Repository
+    For tracking my progression through Rust documentation
+
+## Rust Project Resources & Examples
+    https://github.com/rust-unofficial/awesome-rust
---
+++ chapter-1/main.rs
@@ -0,0 +1,16 @@
+// In every executable Rust program,
+// the main function is always the first code
+// that runs
+fn main() {
+    // println! calls a Rust macro (because of the !)
+    // macros != functions
+    println!("Hello, world!");
+}
+
+// rustc main.rs
+/* 
+    i. Compiling main with the rust compiler 
+    (similar to gcc or clang)
+    ii. Rust outputs a binary executable
+
+*/
